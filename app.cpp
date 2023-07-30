e37c7afc5d27400fb713081abcb58a04e6f225b4
Ethan Mizer <mizerethan32@gmail.com>
2023-07-29 22:49:30 -0400

    Successfully completed tutorial 09! can now do push constant but doubt they will come in handy much. need to keep data alignment in mind because of lack of error reporting.

--- app.cpp
+++ app.cpp
@@ -4,7 +4,17 @@
 #include <stdexcept>
 #include <array>
 
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+
 namespace engine {
+
+    struct SimplePushConstantData {
+        glm::vec2 offset;
+        //super intresting alignment requirements documentation required.
+        alignas(16) glm::vec3 color;
+    };
 
     app::app() {
         loadModels();
@@ -51,12 +61,18 @@ namespace engine {
     }
 
     void app::createPipelineLayout() {
+
+        VkPushConstantRange pushConstantRange {};
+        pushConstantRange.stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT;
+        pushConstantRange.offset = 0;
+        pushConstantRange.size = sizeof(SimplePushConstantData);
+
         VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
         pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
         pipelineLayoutInfo.setLayoutCount = 0;
         pipelineLayoutInfo.pSetLayouts = nullptr;
-        pipelineLayoutInfo.pushConstantRangeCount = 0;
-        pipelineLayoutInfo.pPushConstantRanges = nullptr;
+        pipelineLayoutInfo.pushConstantRangeCount = 1;
+        pipelineLayoutInfo.pPushConstantRanges = &pushConstantRange;
         if(vkCreatePipelineLayout(device.device(), &pipelineLayoutInfo, nullptr, &pipelineLayout) != VK_SUCCESS) {
             throw std::runtime_error("failed to create pipeline layout");
         }
@@ -113,6 +129,8 @@ namespace engine {
     }
 
     void app::recordCommandBuffer(int imageIndex) {
+        static int frame = 0;
+        frame = (frame + 1) % 1000;
         VkCommandBufferBeginInfo beginInfo{};
         beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
 
@@ -128,7 +146,7 @@ namespace engine {
         renderPassInfo.renderArea.extent = swapchain->getSwapChainExtent();
 
         std::array<VkClearValue, 2> clearValues{};
-        clearValues[0].color = {0.1f, 0.1f, 0.1f, 1.0f};
+        clearValues[0].color = {0.01f, 0.01f, 0.01f, 1.0f};
         clearValues[1].depthStencil = {1.0f, 0};
         renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
         renderPassInfo.pClearValues = clearValues.data();
@@ -148,7 +166,15 @@ namespace engine {
 
         pipeline->bind(commandBuffers[imageIndex]);
         model->bind(commandBuffers[imageIndex]);
-        model->draw(commandBuffers[imageIndex]);
+
+        for(int j = 0; j < 4; j++) {
+            SimplePushConstantData push{};
+            push.offset = {-0.5 + frame*0.002f, -0.4f + j * 0.25f};
+            push.color = {0.0f, 0.0f, 0.2f + 0.2f * j};
+
+            vkCmdPushConstants(commandBuffers[imageIndex], pipelineLayout, VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT, 0, sizeof(SimplePushConstantData), &push);
+            model->draw(commandBuffers[imageIndex]);
+        }
         vkCmdEndRenderPass(commandBuffers[imageIndex]);
 
         if(vkEndCommandBuffer(commandBuffers[imageIndex]) != VK_SUCCESS){
