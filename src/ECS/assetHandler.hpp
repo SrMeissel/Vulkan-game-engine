904db1a3dc353c206dd3368c56d14de8758d0818
Ethan Mizer <mizerethan32@gmail.com>
2024-03-11 21:52:43 -0400

    Research, decided on new structure

--- src/ECS/assetHandler.hpp
+++ src/ECS/assetHandler.hpp
@@ -4,12 +4,25 @@
 
 #include "gameObject.hpp"
 
+//this will be my ECS system
+// https://medium.com/@savas/nomad-game-engine-part-2-ecs-9132829188e5 
+//this is great because it has nice pictures 
+//https://austinmorlan.com/posts/entity_component_system/
+//components just hold data. Systems hold logic.
+//std::bitset
+//This is cool ^, real cool 
+
+//im going to replace the current system with one closely based off of austins exaple. Because its nice and I can understand it :)
+//austin suggests using 2 maps, one entity/key pair, and a key/entity pair. why? no clue
+
+//austin uses a type template to register types as component types
+
 namespace engine {
     class assetHandler {
     public:
         assetHandler();
 
-        void getObject(GameObject::id_t key);
+        GameObject getObject(GameObject::id_t key);
 
         //new object
         GameObject::id_t createObject();
