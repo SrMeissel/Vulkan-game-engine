cabc704f60b82c7873bf56d14d2856624c4c30c0
Ethan Mizer <mizerethan32@gmail.com>
2024-03-12 23:30:48 -0400

    Added bare bones of the Entity Manager

--- src/ECS/assetHandler.hpp
+++ src/ECS/assetHandler.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
 #include <vector>
-
-#include "gameObject.hpp"
+#include <array>
+#include <queue>
+#include <bitset>
+#include <cassert>
 
 //this will be my ECS system
 // https://medium.com/@savas/nomad-game-engine-part-2-ecs-9132829188e5 
@@ -15,36 +17,56 @@
 //im going to replace the current system with one closely based off of austins exaple. Because its nice and I can understand it :)
 //austin suggests using 2 maps, one entity/key pair, and a key/entity pair. why? no clue
 
-//austin uses a type template to register types as component types
+//austin uses a type template to register types as components
+
+//using a std::set to store keys for each system to iterate through
+
+//my asset handler class will be my version of the coordinator
+//but will include loading and saving things to disk (Ex. gltf stuff, editor things)
 
 namespace engine {
-    class assetHandler {
+
+using Entity = std::uint32_t;
+static const Entity MAX_ENTITIES = 5000; //#def const? what diff? If I use a vector I dont need this constant
+
+using ComponentType = std::uint8_t;
+const ComponentType MAX_COMPONENTS = 32;
+
+using Signature = std::bitset<MAX_COMPONENTS>; // used to determine which components an entity has
+
+    class EntityManager {
     public:
-        assetHandler();
+        EntityManager() {
+            // The queue with all possible entities must be initialized. I think I can also use a static iteration function like the GameObject constructor but same thing
+            for(Entity entity = 0; entity < MAX_ENTITIES; ++entity) { //why not entity++? 
+                availableEntities.push(entity);
+            }
+        };
 
-        GameObject getObject(GameObject::id_t key);
+        Entity createEntity() {
+            assert(livingEntities < MAX_ENTITIES && "TOO MANY THINGS!!");
+            
+            Entity id = availableEntities.front();
+            availableEntities.pop();
+            livingEntities++;
 
-        //new object
-        GameObject::id_t createObject();
-        //delete object
-        void deleteObject(GameObject::id_t key);
-        //modify object
-            //add component
-                void setModel(GameObject::id_t key);
-                void setTexture(GameObject::id_t key);
-                void setPhysicsObject(GameObject::id_t key);
-                //...
-            //modify component
-            //remove component
+            return id;
+        };
 
-        //save map to disk
-        //load map from disk
+        void destroyEntity(Entity entity) {
+            assert(entity < MAX_ENTITIES && "Entity can never have existed");
+
+            signatures[entity].reset();
+
+            //keeps data density by pushing!!!
+            availableEntities.push(entity);
+            --livingEntities;
+        }
+
     private:
-        //unordered map of gameobjects
-        
-        // lists of keys for objects that have specific components
-        std::vector<GameObject::id_t> modelKeys;
-        std::vector<GameObject::id_t> textureKeys;
-        std::vector<GameObject::id_t> physicsKeys;
+        std::array<Signature, MAX_ENTITIES> signatures{}; //could be vector. // each bit represents a component a entity has. // Will need to be changed if I want to have multiple of the same type of component, which I likely will.
+        std::queue<Entity> availableEntities{}; 
+
+        uint32_t livingEntities{}; //notice same type as Entity
     };
 }
